2f1be50a98f264365028a76fec3c573839e850bb
elvin-osmanov <elvinfo@code.edu.az>
2022-06-15 15:42:22 +0100

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../src/pages/Dashboard";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
