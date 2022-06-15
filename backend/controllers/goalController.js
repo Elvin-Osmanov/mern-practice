3fa6368da5bdcd1f168a00ecbf3c63f3c121490e
elvin-osmanov <elvinfo@code.edu.az>
2022-06-15 15:44:39 +0100

    backend fix

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -41,14 +41,12 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.finById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
-  if (goal.user.toStrin() != user.id) {
+  if (goal.user.toStrin() != req.user.id) {
     res.status(401);
     throw new Error("Unathorized");
   }
@@ -73,14 +71,12 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.finById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
-  if (goal.user.toStrin() != user.id) {
+  if (goal.user.toStrin() != req.user.id) {
     res.status(401);
     throw new Error("Unathorized");
   }
